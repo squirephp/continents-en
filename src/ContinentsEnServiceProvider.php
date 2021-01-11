0fb79010aa41db8e5dc7dd9b94ab27da9a396750
Dan Harrin <dan@danharrin.com>
2021-01-11 08:50:47 +0000

    wip

---
+++ src/ContinentsEnServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Squire;
+
+use Illuminate\Support\ServiceProvider;
+use Squire\Models\Continent;
+
+class ContinentsEnServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Repository::registerSource(Continent::class, 'en', __DIR__.'/../resources/data.csv');
+    }
+}
